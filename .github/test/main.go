8a052f22a9b012310d0b12912d5565f60ad1047d
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2022-12-19 12:03:04 +1300

    Add `_variableWithUnderscore`

--- .github/test/main.go
+++ .github/test/main.go
@@ -7,6 +7,8 @@ var ExportedVariableWithNoComment = "Hello World"
 // this is not a vaild godoc comment
 var ExportedVariableWithIncorrectComment = ExportedVariableWithNoComment
 
+var _variableWithUnderscore = ExportedVariableWithNoComment
+
 func main() {
   fmt.Println(ExportedVariableWithNoComment)
 }
